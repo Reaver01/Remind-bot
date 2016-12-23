c74f033d574058c7f8d09382b4e3653349cacf70
Reaver01 <reaver_01@gmx.com>
2016-12-23 17:20:45 +0100

    shorten

--- lib/bot/extras/cron.rb
+++ lib/bot/extras/cron.rb
@@ -4,8 +4,7 @@ def cronjobs_start
 		clock=Time.new
 		puts "[#{clock.inspect}] Checking for reminders"
 
-		Dir["/botfiles/reminders/*"].each { |file|
-			puts file		
+		Dir["/botfiles/reminders/*"].each {|file| puts file	}
 #			userreminders = loadArr(userreminders, file)
 
 #			pos = 0
@@ -19,7 +18,6 @@ def cronjobs_start
 
 #				pos += 2
 #			end while pos < userreminders.length
-		}
 
 	end
 	scheduler.cron '5 */3 * * *' do
