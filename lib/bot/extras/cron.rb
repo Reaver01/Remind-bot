2458bc2b099e2844fa2dc295d50b8cf027d4e4f4
Reaver01 <reaver_01@gmx.com>
2016-12-23 15:00:49 +0100

    put time in reminder check

--- lib/bot/extras/cron.rb
+++ lib/bot/extras/cron.rb
@@ -1,7 +1,8 @@
 def cronjobs_start
 	scheduler = Rufus::Scheduler.new
 	scheduler.cron '*/1 * * * *' do
-		puts 'checking reminders'
+		clock=Time.new
+		puts "[#{clock.inspect}] Checking for reminders"
 	end
 	scheduler.cron '5 */3 * * *' do
 		$bot.stop
