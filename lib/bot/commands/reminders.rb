3c9b6f3a00661102c85f5c513f760e46f5039244
Reaver01 <reaver_01@gmx.com>
2016-12-24 02:32:12 +0100

    remove reminder text since it errors out every time

--- lib/bot/commands/reminders.rb
+++ lib/bot/commands/reminders.rb
@@ -25,12 +25,10 @@ module MainBot
 						if number == "all"
 							File.delete("botfiles/reminders/#{userid}")
 						else
-							x = number.to_i
-							output = "The following reminder was deleted:\n    Reminder #{x}: @ #{userreminders[userreminders.delete_at((x-1)*2+1)]}"
 							userreminders.delete_at((number.to_i-1)*2+1)
 							userreminders.delete_at((number.to_i-1)*2)
 							File.write("botfiles/reminders/#{event.user.id}", userreminders)
-							event << output
+							event << "Reminder was deleted"
 						end
 					else
 						remindernum = userreminders.length.to_i / 2
