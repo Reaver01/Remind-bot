801641bdf861afbbc95a8d1f40f2aa437cc9ab70
Reaver01 <reaver_01@gmx.com>
2017-01-02 01:19:32 +0100

    fix

--- lib/bot.rb
+++ lib/bot.rb
@@ -12,7 +12,7 @@ module MainBot
 	require 'time_difference'
 	require 'yaml'
 	include Sys
-	require_relative 'lib/bot/class/loader'
+	require_relative 'bot/class/loader'
 	require_relative 'bot/extras/cron'
 	Dir["lib/bot/commands/*.rb"].each {|file| require_relative file }
 
