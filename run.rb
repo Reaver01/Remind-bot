6e16324b945f3c2467a81aea400545335ea94803
Reaver01 <reaver_01@gmx.com>
2017-01-02 01:23:34 +0100

    fix cron

--- run.rb
+++ run.rb
@@ -11,7 +11,7 @@ require 'time'
 require 'time_difference'
 require 'yaml'
 include Sys
-require_relative 'bot/extras/cron'
+require_relative 'lib/bot/extras/cron'
 Dir["lib/bot/commands/*.rb"].each {|file| require_relative file }
 require_relative 'lib/bot/class/loader'
 require_relative 'lib/bot'
