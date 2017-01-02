5843804cfa2c8e1c1b86f3a040f19b01c96c5a0a
Reaver01 <reaver_01@gmx.com>
2017-01-02 02:52:36 +0100

    put client id from env

--- lib/bot/commands/invite.rb
+++ lib/bot/commands/invite.rb
@@ -7,7 +7,7 @@ module MainBot
 					description: "Invites bot to your server",
 					useage: "invite"
 			) do |event|
-				event.respond  "Invite Link: <https://discordapp.com/oauth2/authorize?client_id=261815705811288064&scope=bot&permissions=0>"
+				event.respond  "Invite Link: <https://discordapp.com/oauth2/authorize?client_id=#{ENV['CLIENT']}&scope=bot&permissions=0>"
 				puts "#{event.timestamp}: #{event.user.name}: CMD: invite"
 				nil
 			end
