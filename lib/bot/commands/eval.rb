7e4a006ac01a75f74efc68207150a1ebc23fe249
Reaver01 <reaver_01@gmx.com>
2017-01-02 02:50:43 +0100

    add eval command

---
+++ lib/bot/commands/eval.rb
@@ -0,0 +1,22 @@
+module MainBot
+	module Commands
+		module Eval
+			extend Discordrb::Commands::CommandContainer
+			command(
+					:eval,
+					description: "Evaluates code.",
+					useage: "eval <code>",
+					help_available: false,
+					permission_level: 800,
+					permission_message: "I'm sorry Dave, I cannot do that.",
+			) do |event, *code|
+				puts "#{event.timestamp}: #{event.user.name}: CMD: eval <#{code.join(' ')}>"
+				begin
+					eval code.join(' ')
+				rescue StandardError => e
+					event.respond(e.to_s)
+				end
+			end
+		end
+	end
+end
