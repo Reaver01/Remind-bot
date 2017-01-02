e18375470a8b83917853fb56b0bf637a74022d84
Reaver01 <reaver_01@gmx.com>
2017-01-02 01:28:02 +0100

    remove things from loader

--- lib/bot/class/loader.rb
+++ lib/bot/class/loader.rb
@@ -11,63 +11,6 @@ def loadArr(ar,loc)
 	return ar
 end
 
-#Load object arrays
-def loadObj(ar,loc)
-	if File.exist?(loc)
-		f = File.open(loc,"r")
-		ar=YAML.load(f)
-		puts "Loaded #{loc}"
-		f.close
-	else
-		puts "No file #{loc} to load!"
-	end
-end
-#save object arrays
-def saveObj(ar,loc)
-	ar.sort! { |a,b| a.name.downcase <=> b.name.downcase }
-	if File.exist?(loc) then File.open(loc, 'w') {|f| f.write(YAML.dump(ar)) }
-	else File.open(loc, 'w') {|f| f.write(YAML.dump(ar)) } end
-end
-
-def saveGuilds(ar,loc)
-	ar.sort! { |a,b| a.guild_name.downcase <=> b.guild_name.downcase }
-	if File.exist?(loc) then File.open(loc, 'w') {|f| f.write(YAML.dump(ar)) }
-	else File.new(loc, 'w') {|f| f.write(YAML.dump(ar)) } end
-end
-
-
-def loadmess(loc)
-	if File.exist?(loc)
-		f = File.open(loc,"r")
-		$mess=YAML.load(f)
-		puts 'Mess database Loaded!'
-		f.close
-	else
-		puts 'No file to open!'
-	end
-end
-def loadusers(loc)
-	if File.exist?(loc)
-		f = File.open(loc,"r")
-		$users=YAML.load(f)
-		puts 'User database Loaded!'
-		f.close
-	else
-		puts 'No file to open!'
-	end
-end
-
-def loadguilds(loc)
-	if File.exist?(loc)
-		f = File.open(loc,"r")
-		$guilds=YAML.load(f)
-		puts 'Guild database Loaded!'
-		f.close
-	else
-		puts 'No file to open!'
-	end
-end
-
 def getline(loc,line)
 	if File.exist?(loc)
 		f = File.open(loc,"r")
